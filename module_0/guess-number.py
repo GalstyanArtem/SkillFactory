6a97636ee280eba5757752064e7db26e09bd9aa7
GalstyanArtem <hotface@yandex.ru>
2021-05-26 20:03:04 +0300

    module_0

--- module_0/guess-number.py
+++ module_0/guess-number.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 'Загадано число от 1 до 100, необходимо отгадать его за меньшее количество попыток'
-
 number = np.random.randint(1, 101)   # загаданное число
 print("Загадано число от 1 до 100")
 
@@ -18,6 +17,18 @@ def binary_search(number):                 # бинарная функция
             upper_bound = middle - 1
         elif number < middle:
             lower_bound = middle + 1
-    print(f"Вы угадали число {number} за {count} попыток.")
+    return (count)
 
-binary_search(number)
+
+def score_game(binary_search):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(binary_search(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+score_game(binary_search)
